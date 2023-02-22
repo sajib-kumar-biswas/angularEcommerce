37dd4e03510cefcb94131fdb8d2ec94f14d00a2e
sajib-kumar-biswas <36619979+sajib-kumar-biswas@users.noreply.github.com>
2023-02-22 15:51:19 +0600

    3 files modified, 4 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,16 +5,15 @@ import { products } from '../products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  share(name: string) {
+    window.alert(`The product ${name} has been shared!`);
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
